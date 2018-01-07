46856598a0a7d64c27dfc470028d9bd0cceb8d55
Balint Kleri <balint@kleri.hu>
2018-01-07 11:20:26 +0100

    Add unit test for ‘extractValue’

--- test/test.js
+++ test/test.js
@@ -1,17 +1,44 @@
 const should = require('chai').should;
+const extractValue = require('../src/lib/extract');
 const pick = require('../src/lib/pick');
-should();
 
-describe('Picking notification', () => {
+describe('lambda-slack', () => {
 
-  it('picks notification without match rules', () => {
-    const config = [{
-      'name': 'Notification without match rules',
-    }];
-    const notification = pick({}, config);
-    notification.should.be.an('object');
-    notification.should.have.property('name');
-    notification.name.should.equal('Notification without match rules');
+  before(() => {
+    should();
+  });
+
+  describe('Extracting values', () => {
+    it ('returns correct value from object based on path defined with dot notation (and a preceding `$` symbol)', () => {
+      const obj = {
+        alpha: 'α',
+        nested: {
+          beta: 'β',
+          deeper: {
+            gamma: 'γ',
+            evenDeeper: {
+              delta: 'δ',
+            },
+          }
+        },
+      };
+      extractValue(obj, '$.alpha').should.equal('α');
+      extractValue(obj, '$.nested.beta').should.equal('β');
+      extractValue(obj, '$.nested.deeper.gamma').should.equal('γ');
+      extractValue(obj, '$.nested.deeper.evenDeeper.delta').should.equal('δ');
+    });
+  });
+
+  describe('Picking notification', () => {
+    it('picks notification without match rules', () => {
+      const config = [{
+        'name': 'Notification without match rules',
+      }];
+      const notification = pick({}, config);
+      notification.should.be.an('object');
+      notification.should.have.property('name');
+      notification.name.should.equal('Notification without match rules');
+    });
   });
 
 });
