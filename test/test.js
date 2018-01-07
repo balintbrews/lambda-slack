8cf4eb6314a6511ef1b4e4596bcc7a506cedb4ad
Balint Kleri <balint@kleri.hu>
2018-01-07 13:32:32 +0100

    Cover several use cases with unit tests for picing notification

--- test/test.js
+++ test/test.js
@@ -39,6 +39,108 @@ describe('lambda-slack', () => {
       notification.should.have.property('name');
       notification.name.should.equal('Notification without match rules');
     });
+
+    it('picks matching notification with a sole single-value match rule', () => {
+      const config = [{
+        name: 'Build Notification',
+        match: { '$.source': ['aws.codebuild'] },
+      }];
+      const payload = { source: 'aws.codebuild' };
+      const notification = pick(payload, config);
+      notification.should.be.an('object');
+      notification.should.have.property('name');
+      notification.name.should.equal('Build Notification');
+    });
+
+    it('does not pick non-matching notification', () => {
+      const config = [{
+        match: { '$.source': ['aws.codepipeline'] },
+      }];
+      const payload = { source: 'aws.codebuild' };
+      const notification = pick(payload, config);
+      notification.should.be.a('boolean');
+      notification.should.equal(false);
+    });
+
+    it('picks matching notification with a multi-value match rule', () => {
+      const config = [{
+        name: 'Build Notification',
+        match: {
+          '$.source': ['aws.codebuild', 'aws.codepipeline'],
+        },
+      }];
+      const payload = { source: 'aws.codebuild' };
+      const notification = pick(payload, config);
+      notification.should.be.an('object');
+      notification.should.have.property('name');
+      notification.name.should.equal('Build Notification');
+    });
+
+    it('does not pick notification when not all match rules are fulfilled', () => {
+      const config = [{
+        name: 'Build Notification',
+        match: {
+          '$.source': ['aws.codebuild', 'aws.codepipeline'],
+          '$.detail-type': ['CodeBuild Build State Change'],
+        },
+      }];
+      const payload = {
+        source: 'aws.codebuild',
+        'detail-type': 'CodeBuild Build Phase Change',
+      };
+      const notification = pick(payload, config);
+      notification.should.be.a('boolean');
+      notification.should.equal(false);
+    });
+
+    it('skips non-matching notification and picks notification that matches', () => {
+      const config = [
+        {
+          name: 'Build Phase Notification',
+          match: {
+            '$.source': ['aws.codebuild'],
+            '$.detail-type': ['CodeBuild Build Phase Change'],
+          },
+        },
+        {
+          name: 'Build State Notification',
+          match: {
+            '$.source': ['aws.codebuild'],
+            '$.detail-type': ['CodeBuild Build State Change'],
+          },
+        }
+      ];
+      const payload = {
+        source: 'aws.codebuild',
+        'detail-type': 'CodeBuild Build State Change',
+      };
+      const notification = pick(payload, config);
+      notification.should.be.an('object');
+      notification.should.have.property('name');
+      notification.name.should.equal('Build State Notification');
+    });
+
+    it('picks first notification when there are multiple matching ones', () => {
+      const config = [
+        {
+          name: 'Build Notification — first',
+          match: {
+            '$.source': ['aws.codebuild', 'aws.codepipeline'],
+          },
+        },
+        {
+          name: 'Build Notification — second',
+          match: {
+            '$.source': ['aws.codebuild'],
+          },
+        }
+      ];
+      const payload = { source: 'aws.codebuild' };
+      const notification = pick(payload, config);
+      notification.should.be.an('object');
+      notification.should.have.property('name');
+      notification.name.should.equal('Build Notification — first');
+    });
   });
 
 });
