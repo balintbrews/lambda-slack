39230c1fda725714068c612efce33c9ec6a40589
Balint Kleri <balint@kleri.hu>
2018-01-22 00:34:15 +0100

    Validate if sample payload file’s path is provided for test run script

--- src/test_run.js
+++ src/test_run.js
@@ -2,6 +2,11 @@ const fs = require('fs');
 const { handler } = require('./index.js');
 const path = require('path');
 
+if (process.argv.length !== 3) {
+  console.error(`Please provide sample payload file's path as an argument. Usage: node ${process.argv[1]} path-to-payload.json`);
+  return;
+}
+
 const payloadFilePath = path.join(process.cwd(), process.argv[2]);
 console.log(`Using payload from file: ${payloadFilePath}`);
 
